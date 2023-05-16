c29defc34e4355752a1da9fe0b89249066624dd7
mayhim182 <mayankatnitrkl@gmail.com>
2023-05-16 11:45:05 +0530

    added links

--- src/app/mainScreen/home/home.component.ts
+++ src/app/mainScreen/home/home.component.ts
@@ -5,31 +5,6 @@ import { trigger,state,style,animate,transition } from '@angular/animations';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  animations: [
-    trigger('moveImages', [
-      state('left', style({
-        left: '0'
-      })),
-      state('right', style({
-        left: 'calc(100% - 150px)'
-      })),
-      transition('left <=> right', [
-        animate('1s')
-      ])
-    ])
-  ]
 })
 export class HomeComponent{
-
-  
-  ngOnInit() {
-    setInterval(() => this.moveImages(), 2000);
-  }
-
-  animationState = 'left';
-
-  moveImages() {
-    this.animationState = this.animationState === 'left' ? 'right' : 'left';
-  }
-
 }
